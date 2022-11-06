0b16d5310e150cde3fd7a59dd443227ba40234d3
Rashad Long <RDLong718@gmail.com>
2022-11-06 01:26:44 -0400

    initial commit

---
+++ vue_master_class/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="container">
+    <page-home />
+  </div>
+</template>
+
+<script>
+  import PageHome from "@/components/PageHome.vue";
+
+  export default {
+    name: "App",
+    components: {
+      PageHome,
+    },
+  };
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
